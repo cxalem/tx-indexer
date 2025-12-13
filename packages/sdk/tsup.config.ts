50552b4d7c369baad75e561635b15b68249231dc
cxalem <smarketing.cuz@gmail.com>
2025-12-13 03:08:18 +0100

    npm stuff

---
+++ packages/sdk/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/client.ts', 'src/types.ts'],
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  treeshake: true,
+  splitting: false,
+  // Don't bundle peer dependencies
+  external: ['@solana/kit', 'zod'],
+  // Bundle all workspace dependencies into the output
+  noExternal: [/^@tx-indexer/],
+});
+
